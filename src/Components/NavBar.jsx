f62a365042e1d6519217e682c3c3c882400cb48f
alexamber2021@gmail.com <alexamber2021@gmail.com>
2025-05-26 12:46:27 -0700

    Plant list second  commit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,10 +4,8 @@ import logo from "../assets/images/header-logo.png";
 const NavBar = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-2">
-    <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-        {/* <h1 class="m-0">Gardener</h1> */}
-          <img src={logo} alt="Peels Native Plants" className="header-logo" />
-    </a>
+        <Link to="/" class="navbar-brand d-flex align-items-center px-4 px-lg-5"><img src={logo} alt="Peels Native Plants" className="header-logo" /></Link>
+   
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
     </button>
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from 'react-router-dom';
 import "../main.css"; // Ensure you have global styles
 import logo from "../assets/images/header-logo.png"; // Update path if using public folder
 
@@ -7,8 +7,8 @@ const Header = () => {
     <header className="header">
       <div className="header-container">
         {/* Logo */}
-        <img src={logo} alt="Peels Native Plants" className="header-logo" />
-
+        
+           <Link to="/" class="nav-item nav-link"><img src={logo} alt="Peels Native Plants" className="header-logo" /></Link>
         {/* Navigation Menu */}
         <nav className="nav-menu">
           <ul>
