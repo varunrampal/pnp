7b4f75cdf31fb54ba3dde50710b828750a1888db
alexamber2021@gmail.com <alexamber2021@gmail.com>
2025-08-14 09:14:53 -0700

    popup-blocked

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   return (
       
     <>
-    <AutoPopup/>
+    {/* <AutoPopup/> */}
     <TopBar/>
      <Router>
       <NavBar/>
@@ -32,7 +32,7 @@ const App = () => {
             <Route path="/Quote" element={<Quote />} />   
             <Route path="/Plants" element={<Plants />} />  
             <Route path="/dragdrop" element={<DragDrop/>} />  
-            plantstest   
+               
           </Routes>
           <BottomFeatures/>
           <Footer/>
@@ -43,5 +43,39 @@ const App = () => {
  </>
   );
 };
+
+
+//  const printZPL = async () => {
+//     const zpl = `
+//       ^XA
+//       ^FO50,50^A0N,50,50^FDHello Zebra ZT231^FS
+//       ^FO50,120^BY2
+//       ^BCN,100,Y,N,N
+//       ^FD1234567890^FS
+//       ^XZ
+//     `;
+
+//     try {
+//       // NOTE: This fetch will only work if printer supports HTTP printing directly
+//       await fetch("http://192.168.1.100:9100", {
+//         method: "POST",
+//         body: zpl,
+//         headers: {
+//           "Content-Type": "text/plain"
+//         }
+//       });
+//       alert("Label sent to printer.");
+//     } catch (err) {
+//       console.error("Print failed:", err);
+//       alert("Failed to print label.");
+//     }
+//   };
+
+//   return (
+//     <button onClick={printZPL} className="bg-blue-600 text-white px-4 py-2 rounded">
+//       Print Label
+//     </button>
+//   );
+//};
 
 export default App;
